863047806394639ab5098e1d946fea3e05160ce6
Jakob Ondrey <ondrey@gmail.com>
2021-08-25 21:09:35 -0500

    mods job to run hourly and modifies script to account for increase in frequency.

--- csv_update/app.py
+++ csv_update/app.py
@@ -36,7 +36,6 @@ def downloadNewData (): # returns data as panndas.df
     # will need to some it with a new way of validating
     #newDataQuialityControl(freshurl)
 
-    # prob wont be able to add to a df now. Will need to do a for loop through the list, 
 
 
     fresh = pd.read_csv(freshurl)
@@ -46,72 +45,103 @@ def downloadNewData (): # returns data as panndas.df
 
     return (fresh)
 
-def updateOldData(fresh):
-    olddf = pd.read_csv("https://s3.amazonaws.com/cpscovid.com/data/allCpsCovidData.csv")
-    oldtotals = pd.read_csv("https://s3.amazonaws.com/cpscovid.com/data/CPStotals.csv")
-    
-    # make list of acceptable school IDs
-    schoolIDList = olddf.CPS_School_ID.tolist()
+def checkLastColumn(olddf, formated, updateChecker):
+    # format todays date. )
+    if formated not in olddf.columns:
+        olddf[formated] = 0
+        updateChecker = True
+        return olddf, updateChecker
+    else:
+        return olddf, updateChecker
 
+def updateOldDf(olddf, fresh, formated, updateChecker):
     # Total all cases from each school
+    # how do we skip this if there are not new numbers?
+    # for now just keep it
     freshTotals = Counter(fresh['CPS School ID'])
-
-    # lambda uses UTC so numbers checked "today" will be for "yesterday" 
-    # Determines date makes new column and populates with 0
-    yesterday = datetime.today() - timedelta(days=1)
-    formated = yesterday.strftime("%Y%m%d")
-    olddf[formated] = 0
-
     # determines the column indexes for tail sums 
     end = len(olddf.columns)
     d7 = end - 7
     d14 = end - 13
-
     # Primary df update
     for index, row in olddf.iterrows():
         # determines if there is and difference between old totals and fresh totals
         if row['gTotal'] != freshTotals[row['CPS_School_ID']]:
             #updates daily number and total
-            olddf.at[index,formated]= freshTotals[row['CPS_School_ID']] - row['gTotal']
+            updateChecker = True
+            olddf.at[index,formated] = freshTotals[row['CPS_School_ID']] - row['gTotal'] + row[formated]
             olddf.at[index,['gTotal']] = freshTotals[row['CPS_School_ID']]
+
         # updates 7 and 14 day totals regardless of new
+        # 
         olddf.at[index, ['7Total']] = olddf.iloc[index, d7:end].sum()
         olddf.at[index, ['14Total']] = olddf.iloc[index, d14:end].sum()
 
-    # update cpstotals
-    newdaily = olddf[formated].sum()
+    return olddf, updateChecker
+def updateOldTotals(oldtotals, newdaily, formated):
+
+    if str(oldtotals.at[len(oldtotals)-1,'date']) == formated:
+        oldtotals.drop(oldtotals.tail(1).index,inplace=True)
+
     newrunning = oldtotals['daily'].sum() + newdaily
     new7DaySum = (oldtotals['daily'].tail(6).sum() + newdaily)
     new7DayAvg = new7DaySum / 7
     new14DaySum = (oldtotals['daily'].tail(13).sum() + newdaily)
     new14DayAvg = new14DaySum / 14
-
     newrow = [formated, newdaily, newrunning, new7DaySum, new7DayAvg, new14DaySum, new14DayAvg]
     oldtotals.loc[len(oldtotals)] = newrow
 
-    # make transposed df for easier parsing for front end maybe make a function
-    begindex = deque(list(olddf.columns))
+    return oldtotals
+
+def transposeDf (df):
+    begindex = deque(list(df.columns))
     begindex.popleft() 
 
-
-    transposed = olddf.transpose()
+    transposed = df.transpose()
     new_header = transposed.iloc[0]
     transposed = transposed[1:]
     transposed.columns = new_header
     transposed.insert(0, 'School', begindex, allow_duplicates = True)
+    return transposed
+def updateOldData(fresh):
+    updateChecker = False
+    time = datetime.now() - timedelta(hours=5)
+    formated = time.strftime("%Y%m%d")
 
-    # export to csv
-    exportUpdated(olddf, 'allCpsCovidData.csv')
-    exportUpdated(oldtotals, 'CPStotals.csv')
-    exportUpdated(transposed, 'newFormatTest.csv')
+    olddf = pd.read_csv("https://s3.amazonaws.com/cpscovid.com/data/allCpsCovidData.csv")
+    oldtotals = pd.read_csv("https://s3.amazonaws.com/cpscovid.com/data/CPStotals.csv")
+    
+    # ensures today's date is in olddf
+    olddf, updateChecker = checkLastColumn(olddf, formated, updateChecker)
+
+    # this function will update the master list of all cases 
+    olddf, updateChecker = updateOldDf (olddf, fresh, formated, updateChecker)
+
+    if updateChecker:
+        # update cpstotals
+        newdaily = olddf[formated].sum()
+        oldtotals = updateOldTotals(oldtotals, newdaily, formated)
+
+
+        # make transposed df for easier parsing for front end maybe make a function
+        transposed = transposeDf(olddf)
+
+
+        # export to csv
+        exportUpdated(olddf, 'allCpsCovidData1.csv')
+        exportUpdated(oldtotals, 'CPStotals1.csv')
+        exportUpdated(transposed, 'newFormatTest1.csv')
+    else:
+        logger.info("no update")
 
 def exportUpdated(updated, fileName):
     csv_buffer = StringIO()
     updated.to_csv(csv_buffer, index=False)
     putLocation = "cpscovid.com"
     putKey = "data/" + fileName
+    tagging = "lifecycle=true"
     logger.info(fileName)
-    response = s3client.put_object(Body=csv_buffer.getvalue(), Bucket=putLocation, Key=putKey)
+    response = s3client.put_object(Body=csv_buffer.getvalue(), Bucket=putLocation, Key=putKey, Tagging=tagging)
     logger.info(response)
 
 def findCSVDate(date):
