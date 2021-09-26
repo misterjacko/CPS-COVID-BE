9b6c4b949b8ae0414740c0e892a5bb091ef60c4d
Jakob Ondrey <ondrey@gmail.com>
2021-09-26 10:42:08 -0500

    fixes pipeline

--- csv_update/app.py
+++ csv_update/app.py
@@ -28,28 +28,9 @@ def newDataQuialityControl(freshurl):
     assert fresh[fresh.columns[0]].equals(totalsdf[totalsdf.columns[0]]), "Table order rearranged!"
 
 def downloadNewData (): # returns data as panndas.df
-    # old sheet
-    # sheetID = '1dMtr8hhhKjPyyNg7i6V52iMQXEqa67E9iAmECeOqZ6c'
-    # worksheetName = 'COVID' # only takes first word fo the sheet
-    # case data
-    sheetID = '1urvD6F5RD3U3VHImFc5Kfv63lV7UQ9ivv_HpiFobGVA'
-    worksheetName = 'case' # only takes first word fo the sheet
-    freshurl = 'https://docs.google.com/spreadsheets/d/{0}/gviz/tq?tqx=out:csv&sheet={1}'.format(
-        sheetID,
-        worksheetName
-    )
-
-    # validate new data
-    # will need to some it with a new way of validating
-    #newDataQuialityControl(freshurl)
-
-
-
-    fresh = pd.read_csv(freshurl)
-
-    # remove entries with no school ID
-    fresh = fresh.dropna(subset=['CPS School ID'])
-
+    start_date = "8-29-2021"
+    response = requests.get("	https://api.cps.edu/health/cps/schoolWeeklyCovidActionable?startdate={0}".format(start_date))
+    fresh = response.json()
     return (fresh)
 
 def checkLastColumn(olddf, formated, updateChecker):
@@ -66,24 +47,40 @@ def updateOldDf(olddf, fresh, formated, updateChecker, updateNumbers):
     # how do we skip this if there are not new numbers?
     # for now just keep it
     newCaseDict = {}
-    freshTotals = Counter(fresh['CPS School ID'])
+    # freshTotals = Counter(fresh['CPS School ID'])
+
+    #makes a dict of school totals with schoolid = casetotals
+    freshTotals = {}
+    for week in fresh:
+        if week['SchoolID'] not in freshTotals:
+            freshTotals[week['SchoolID']] = week['TotalCaseCount']
+        else:
+            freshTotals[week['SchoolID']] += week['TotalCaseCount']
+    # print(freshTotals)
     # determines the column indexes for tail sums 
     end = len(olddf.columns)
     d7 = end - 7
     d14 = end - 13
+    d21 = end - 20
     # Primary df update
+
     for index, row in olddf.iterrows():
+        if row['CPS_School_ID'] not in freshTotals:
+            continue
         # determines if there is and difference between old totals and fresh totals
-        if row['gTotal'] != freshTotals[row['CPS_School_ID']]:
+        if row['gTotal'] - row['preSY2122'] != freshTotals[row['CPS_School_ID']]:
+            yearTotal = row['gTotal'] - row['preSY2122']
+            # print("not")
             #updates daily number and total
             updateChecker = True
             updateNumbers = True
 
-            olddf.at[index,formated] = freshTotals[row['CPS_School_ID']] - row['gTotal'] + row[formated]
-            olddf.at[index,['gTotal']] = freshTotals[row['CPS_School_ID']]
+            olddf.at[index,formated] = freshTotals[row['CPS_School_ID']] - yearTotal + row[formated]
+            olddf.at[index,['gTotal']] = freshTotals[row['CPS_School_ID']] + row['preSY2122']
 
         olddf.at[index, ['7Total']] = olddf.iloc[index, d7:end].sum()
         olddf.at[index, ['14Total']] = olddf.iloc[index, d14:end].sum()
+        olddf.at[index, ['21Total']] = olddf.iloc[index, d21:end].sum()
     if updateNumbers:
         for index, row in olddf.iterrows():
             if row[formated] != 0:
@@ -122,8 +119,9 @@ def transposeDf (df):
     transposed.insert(0, 'School', begindex, allow_duplicates = True)
     return transposed
 
-def formatSNS(data):
-    dataString = "<tr><th>School</th><th>New Cases</th></tr>"
+def formatSNS(data, table_date):
+    dataString = "<caption>Date: {0}</caption>".format(table_date)
+    dataString = "{0}<tr><th>School</th><th>New Cases</th></tr>".format(dataString)
     schoolCount = len(data)
     caseCount = 0
     for item in data:
@@ -132,13 +130,14 @@ def formatSNS(data):
         linkHTML = '<a href=./school.html{0}>{1}</a>'.format(schoolURL, item)
         line = '<tr><td>{0}</td><td>{1}</td></tr>'.format(linkHTML, data[item][0])
         dataString = '{0}{1}'.format(dataString, line)
-
     sns_string = 'cpscovid.com has detected new COVID-19 cases.\n\n'
     sns_string = '{0}{1} new cases reported.\n'.format(sns_string, caseCount)
     sns_string = '{0}{1} schools affected.\n\n'.format(sns_string, schoolCount)
-    sns_string = '{0}View a list of newly published cases at cpscovid.com/newcases.html'.format(sns_string)
+    sns_string = '{0}View a list of newly published cases at cpscovid.com/newcases.html\n\n'.format(sns_string)
+    sns_string = '{0}Follow @CPSCovid on twitter at twitter.com/CPSCovid'.format(sns_string)
 
     tweet_string = 'Case numbers updated by @ChiPubSchools.\n\n'
+    tweet_string = '{0}Cumulative cases reported {1}\n'.format(tweet_string, table_date)
     tweet_string = '{0}New cases: {1}\n'.format(tweet_string, caseCount)
     tweet_string = '{0}Schools affected: {1}\n\n'.format(tweet_string, schoolCount)
     tweet_string = '{0}View the list of cases at cpscovid.com/newcases.html'.format(tweet_string)
@@ -149,6 +148,7 @@ def updateOldData(fresh):
     updateNumbers = False
     time = datetime.now() - timedelta(hours=5)
     formated = time.strftime("%Y%m%d")
+    table_date = time.strftime("%b %d %Y")
 
     olddf = pd.read_csv("https://s3.amazonaws.com/cpscovid.com/data/allCpsCovidData.csv")
     oldtotals = pd.read_csv("https://s3.amazonaws.com/cpscovid.com/data/CPStotals.csv")
@@ -156,8 +156,8 @@ def updateOldData(fresh):
     # ensures today's date is in olddf
     olddf, updateChecker = checkLastColumn(olddf, formated, updateChecker)
 
-    # this function will update the master list of all cases 
-    olddf, updateChecker, updateNumbers, newCaseDict = updateOldDf (olddf, fresh, formated, updateChecker,updateNumbers)
+    # this function will update the master list of all cases
+    olddf, updateChecker, updateNumbers, newCaseDict = updateOldDf (olddf, fresh, formated, updateChecker, updateNumbers)
 
     if updateChecker:
         # update cpstotals
@@ -172,13 +172,16 @@ def updateOldData(fresh):
         exportUpdated(oldtotals, 'CPStotals.csv')
         exportUpdated(transposed, 'newFormatTest.csv')
         if updateNumbers:          
-            exportUpdated(fresh, 'dataFromCPS.csv')
-            dataString, sns_string, tweet_string = formatSNS(newCaseDict)
+            dataString, sns_string, tweet_string = formatSNS(newCaseDict, table_date)
             exportHtml(dataString)
-            logger.info(sendSNS(sns_string)) # this can be passed an array for subscription tags 
+            logger.info(sendSNS(sns_string)) # this requires topicARN and wont work in test
             invalidateCache()
             sendTweet(tweet_string)
-            # logger.info(tweet_string)
+            logger.info(tweet_string)
+            logger.info(sns_string)
+
+            freshdf = pd.DataFrame.from_dict(fresh, orient='columns')
+            exportUpdated(freshdf, 'dataFromCPS.csv')
         
     else:
         logger.info("no update")
@@ -203,6 +206,7 @@ def exportHtml(dataString):
     soup = bs4(page, 'html.parser')
 
     putLocation = 'cpscovid.com'
+    # putKey = "newcasestest.html"
     putKey = "newcases.html"
     tagging = "lifecycle=true"
     acl = 'public-read'
